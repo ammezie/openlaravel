e70f8b63b28195e96d3bc4940c7efbd32a1c4524
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-23 17:44:48 +0100

    add slug column to projects table

--- database/migrations/2016_07_23_122307_create_projects_table.php
+++ database/migrations/2016_07_23_122307_create_projects_table.php
@@ -15,6 +15,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->string('slug');
             $table->string('project_url');
             $table->string('repo_url')->unique();
             $table->string('packagist_url')->nullable();
