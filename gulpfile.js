31810746acc9f7810bf26071de3416f31e4be53c
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-23 13:02:15 +0100

    remove material design

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.copy('node_modules/bulma/sass', 'resources/assets/sass')
-    	.copy('node_modules/bulma/bulma.sass', 'resources/assets/sass/bulma.sass');
+    	// .copy('node_modules/bulma/bulma.sass', 'resources/assets/sass/bulma.sass');
 
 
 });
 
-// elixir(function(mix) {
-// 	mix.scripts(['vendor/jquery.js', 'vendor/materialize.js'], 'public/js/vendor.js');
-// })
+elixir(function(mix) {
+	mix.scripts('app.js', 'public/js/app.js');
+})
 
 elixir(function(mix) {
     mix.sass('app.sass');
