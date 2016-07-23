37979c7c63894da1c26cfd9b28c37317e217ece6
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-23 13:16:35 +0100

    remove Laravel default welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="A repository of open source Laravel projects.">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  
   <title>Open Laravel</title>
 
   {{-- <link rel="shortcut icon" href="images/favicon.png"> --}}
