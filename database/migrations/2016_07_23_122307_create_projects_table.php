9e1e574b454af5070a5257129d1ee8ce8964fc17
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-23 13:45:14 +0100

    remove default Laravel users migrations

--- database/migrations/2016_07_23_122307_create_projects_table.php
+++ database/migrations/2016_07_23_122307_create_projects_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('project_url');
+            $table->string('repo_url')->unique();
+            $table->string('packagist_url')->nullable();
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('projects');
     }
 }
