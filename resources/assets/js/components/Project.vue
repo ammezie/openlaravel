ab1be6fa1834eb95c1b0c6d82541fb8d4fa8fa0f
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-07-27 23:19:08 +0100

    setup vuejs

---
+++ resources/assets/js/components/Project.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="columns is-multiline">
+		<div class="column is-one-third">
+	      <div class="card is-fullwidth">
+	        <a href="projects/{{ project.slug }}">
+		        <div class="card-content has-text-centered">
+		          <div class="content">
+			        <h3 class="title">
+			        	{{ project.title }}
+			        </h3>
+
+			        <small>{{ project.slug }}</small>
+
+		          	{{ project.description }}
+		          </div>
+		        </div>
+	        </a>
+	      </div>
+	    </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+
+</style>
