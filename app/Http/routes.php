74e523072c8c7c148aa1cc3a59c97c8e41a783e8
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-08-07 14:19:25 +0100

    remove register routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,16 @@
 | Web Routes
 |---------------------------------------------------------------------------
 */
+// Authentication Routes...
+$this->get('login', 'Auth\AuthController@showLoginForm');
+$this->post('login', 'Auth\AuthController@login');
+$this->get('logout', 'Auth\AuthController@logout');
+
+// Password Reset Routes...
+$this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+$this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+$this->post('password/reset', 'Auth\PasswordController@reset');
+
 Route::get('/', 'ProjectsController@index');
 
 Route::get('submit-project', 'ProjectsController@create');
@@ -28,5 +38,3 @@ Route::group(['prefix' => 'api', 'namespace' => 'API'], function() {
 	    Route::patch('approve-project/{slug}', 'AdminProjectsController@approveProject');
 	});
 });
-
-Route::auth();
