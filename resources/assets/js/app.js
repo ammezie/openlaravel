1a3a732a4740e70db344b14cfeac0f17f499a271
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-08-19 14:13:57 +0100

    fix error on submiting project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,19 +1,10 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
-// import VuePaginator from 'vuejs-paginator/dist/vuejs-paginator.min.js';
 
 Vue.use(VueResource);
 
 import Project from './components/Project.vue';
 import FormError from './components/FormError.vue';
-import Vuetable from 'vuetable/src/components/Vuetable.vue';
-import VuetablePagination from 'vuetable/src/components/VuetablePagination.vue';
-import VuetablePaginationBulma from './components/VuetablePaginationBulma.vue';
-
-// Vue.component('vuetable', Vuetable);
-Vue.component('vuetable-pagination', VuetablePagination);
-Vue.component('vuetable-pagination-bulma', VuetablePaginationBulma);
-
 
 Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('value');
 
@@ -23,7 +14,6 @@ new Vue({
 	components: {
 		Project,
 		FormError,
-		vuetable: Vuetable,
 	},
 
 	data: {
@@ -40,45 +30,6 @@ new Vue({
 		formSubmitted: false,
 
 		errors: [],
-
-		columns: [
-            {
-                name: 'title',
-                title: 'Project Title',
-
-            },
-
-            {
-                name: 'project_url',
-                title: 'Project URL',
-            },
-
-            {
-                name: 'repo_url',
-                title: 'Repository URL',
-            },
-
-            {
-                name: 'description',
-                title: 'Project Description',
-            },
-
-            {
-                name: 'status',
-                title: 'Status',
-            },
-
-            '__actions'
-        ],
-
-         itemActions: [
-            {
-            	name: 'approve-project',
-            	label: '',
-            	icon: 'fa fa-thumbs-o-up',
-            	class: 'button is-primary'
-            },
-        ]
 	},
 
 	ready: function() {
@@ -91,30 +42,13 @@ new Vue({
 
 			this.$http.get(page_url).then(function(response) {
 				this.$set('projects', response.data);
-				// this.makePagination(response.data);
 			});
 		},
-
-		// getProject(slug) {
-		// 	this.$http.get('api/projects/', slug);
-
-		// },
-
-		// makePagination(data) {
-		// 	let pagination = {
-		// 		current_page: data.current_page,
-		// 		last_page: data.last_page,
-		// 		next_page_url: data.next_page_url,
-		// 		prev_page_url: data.prev_page_url
-		// 	}
-
-		// 	this.$set('pagination', pagination);
-		// },
 
 		createProject() {
 			let project = this.newProject;
 
-			this.projects.push(project);
+			this.$set('projects', project);
 
 			this.newProject = {
 				title: '',
@@ -129,21 +63,6 @@ new Vue({
 			}, function (response) {
 				this.$set('errors', response.data);
 			});
-		},
-
-		approveProject: function(project) {
-            this.$http.patch('api/admin/approve-project/'+project.slug, project).then(function() {
-            	// reload data from server
-            	this.$broadcast('vuetable:reload');
-            });
-        },
+		}
 	},
-
-	events: {
-        'vuetable:action': function(action, project) {
-            if (action == 'approve-project') {
-                this.approveProject(project);
-            }
-        },
-    }
 });
