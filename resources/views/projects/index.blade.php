a2acec676a57736a4b40dace2dd02f13219c6cc1
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-01-29 18:07:31 +0100

    refactor styles

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -14,13 +14,12 @@
 	@foreach ($projects as $project)
 		<div class="column is-one-third">
 			<a href="{{ url('projects/'. $project->slug) }}">
-		    	<div class="card is-fullwidth">
+		    	<div class="card project-card is-fullwidth">
 			    	<div class="card-content has-text-centered">
 			        	<div class="content">
-				        	<h3 class="title">
+			        		<h3 class="title">
 			        			{{ $project->title }}
 			        		</h3>
-
 			        		<div class="description">
 			        			{{ $project->short }}
 			        		</div>
